715f55e110d8d343bc062aa3d45f4321ea28e3d1
jinsu291 <wlstn291@naver.com>
2023-04-20 20:55:56 +0900

    코틀린에서 변수를 다루는 방법

---
+++ src/main/java/com/lannstark/lec01/Lec01Main2.kt
@@ -0,0 +1,12 @@
+package com.lannstark.lec01
+
+fun main() {
+
+    var number1: Long = 10L
+    val number2: Long = 10L
+
+    var number3: Long? = 1_000L
+    number3 = null
+
+    var person = Person("수수")
+}
