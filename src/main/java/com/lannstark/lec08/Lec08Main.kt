f3fa00e33c0c4340165722477ba1d8a27cd72180
jinsu291 <wlstn291@naver.com>
2023-04-21 21:39:59 +0900

    코틀린에서 함수를 다루는 방법

---
+++ src/main/java/com/lannstark/lec08/Lec08Main.kt
@@ -0,0 +1,38 @@
+package com.lannstark.lec05
+
+fun main() {
+    repeat("Hello World", useNewLine = false)
+    printNameAndGender("수수", "FEMALE")
+
+    printAll("A", "B", "C")
+
+    val array = arrayOf("A", "B", "C")
+    printAll(*array)
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+fun repeat(
+    str: String,
+    num: Int = 3,
+    useNewLine: Boolean = true
+) {
+    for (i in 1..num) {
+        if (useNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
+
+fun printNameAndGender(name: String, gender: String) {
+    println(name)
+    println(gender)
+}
+
+fun printAll(vararg strings: String) {
+    for (str in strings) {
+        println(str)
+    }
+}
