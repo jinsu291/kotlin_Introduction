d28a0a14cbe5286e191e57540449678d7c6c64a3
jinsu291 <wlstn291@naver.com>
2023-04-24 17:52:45 +0900

    코틀린에서 object 키워드를 다루는 방법

--- src/main/java/com/lannstark/lec12/Lec12Main.java
+++ src/main/java/com/lannstark/lec12/Lec12Main.java
@@ -2,8 +2,10 @@ package com.lannstark.lec12;
 
 public class Lec12Main {
 
-  public static void main(String[] args) {
-    // Person.Factory.newBaby("ABC");
-  }
+    public static void main(String[] args) {
+        Person.Companion.newBaby("ABC");
+        Person.newBaby("ABC"); // @JvmStatic을 썼을 때
+        // Person.Factory.newBaby("ABC");
+    }
 
 }
